5ed47318ce816244abee388a4941b3a9641ca1a4
JustAMatt <69873176+JustAMatt@users.noreply.github.com>
2021-02-11 20:21:04 -0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,7 +57,7 @@ module.exports = {
           items: [
             {
               label: 'Style Guide',
-              to: 'docs/',
+              to: '',
             },
           ],
         },
