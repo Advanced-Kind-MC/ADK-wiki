b2962a363d2b18c8eaee97c395cdf9b959ecbc3a
Liv Kaminske <71091547+liv-kami@users.noreply.github.com>
2021-02-14 21:39:33 -0500

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'starter_guide', 'money'],
+    Docusaurus: ['doc1', 'starter_guide', 'money', 'rules'],
     Features: ['mdx'],
   },
 };
