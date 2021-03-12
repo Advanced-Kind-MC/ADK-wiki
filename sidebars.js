70cceed33b28ae0210795bc349e0bfcc760452f0
Liv Kaminske <71091547+liv-kami@users.noreply.github.com>
2021-03-11 23:08:35 -0500

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     Introduction: ['starter_guide', 'rules', 'doc1'],
-    Features: ['brewery', 'Mines'],
+    Features: ['brewery', 'Mines', 'AdvancedRestone'],
     Guides: ['money', 'towny', 'recipes', 'Applications'],
     Staff: ['staff_guide'],
   },
