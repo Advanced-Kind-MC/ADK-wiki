7976bb0500a174d517d61703a0d008a68676e56d
JustAMatt <69873176+JustAMatt@users.noreply.github.com>
2021-03-15 17:49:23 -0300

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     Introduction: ['starter_guide', 'rules', 'doc1'],
-    Features: ['brewery', 'Mines', 'AdvancedRestone'],
+    Features: ['brewery', 'Mines', 'AdvancedRedstone'],
     Guides: ['money', 'towny', 'recipes', 'Applications'],
     Staff: ['staff_guide'],
   },
