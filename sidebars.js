cd86d86509bd04427e5dad416fca70a11dd87c06
SirKumsishon <79440338+SirKumsishon@users.noreply.github.com>
2021-05-12 20:20:12 -0700

    Update sidebars.js
    added McMMO flosh

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     Introduction: ['starter_guide', 'rules', 'doc1'],
-    Features: ['brewery', 'Mines', 'AdvancedRedstone'],
+    Features: ['brewery', 'Mines', 'AdvancedRedstone', 'McMMO'],
     Guides: ['money', 'towny', 'recipes', 'Applications', 'Mending'],
     Staff: ['staff_guide'],
   },
