a7a5db8db08c253e1a1d1e1108c5e27f682f56dc
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2022-05-16 23:14:09 -0500

    Changing Mcmmo -> AureliumSkills

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     Introduction: ['starter_guide', 'rules', 'doc1'],
-    Features: ['DeepWild', 'brewery', 'Mines', 'AdvancedRedstone', 'McMMO'],
+    Features: ['DeepWild', 'brewery', 'Mines', 'AdvancedRedstone', 'AureliumSkills'],
     Guides: ['money', 'towny', 'recipes', 'Applications', 'Mending'],
     Staff: ['staff_guide'],
   },
