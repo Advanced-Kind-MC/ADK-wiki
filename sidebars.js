1bde8f9c9f4f46b2071776b88742eff1c90f4f4f
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2022-05-16 23:17:04 -0500

    Removes AdvancedRedstone

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     Introduction: ['starter_guide', 'rules', 'doc1'],
-    Features: ['DeepWild', 'brewery', 'Mines', 'AdvancedRedstone', 'AureliumSkills'],
+    Features: ['DeepWild', 'brewery', 'Mines', 'AureliumSkills'],
     Guides: ['money', 'towny', 'recipes', 'Applications', 'Mending'],
     Staff: ['staff_guide'],
   },
