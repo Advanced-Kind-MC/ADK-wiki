1c15588038fd3dec335342ddae024266af84f6d3
IAmSoccer <therealsoccerball500@gmail.com>
2022-07-30 01:18:39 -0500

    Removes Deep Wild Section. Adds Quest World Section

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     Introduction: ['starter_guide', 'rules', 'doc1'],
-    Features: ['DeepWild', 'brewery', 'Mines', 'AureliumSkills'],
+    Features: ['brewery', 'Mines', 'AureliumSkills', 'quest_world',],
     Guides: ['money', 'towny', 'recipes', 'Applications', 'Mending'],
     Staff: ['staff_guide'],
   },
